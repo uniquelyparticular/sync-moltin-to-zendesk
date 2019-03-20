9c5daaaadd38eee5d3cfa9236616e3455f8c93eb
Adam Grohs <adamgrohs@Adams-MacBook-Air.local>
2019-03-20 13:03:33 -0500

    initial import

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# sync-moltin-to-zendesk
-This example demonstrates how you can sync payment refunds from Moltin into Zendesk Sunshine via Webhook API.
+# @particular./sync-moltin-to-zendesk
+
+> Add a Zendesk Sunshine Evenet when an order is created or modified in Moltin
+
+Asynchronous microservice that is triggered by [moltin](https://moltin.com) webhooks to create a Sunshine Event inside of [Zendesk](https://zendesk.com).
+
+Built with [Micro](https://github.com/zeit/micro)! 🤩
+
+## 🛠 Setup
+
+Both a [Zendesk](https://zendesk.com) _and_ [moltin](https://moltin.com) account are needed for this to function.
+
+Create a `.env` at the project root with the following credentials:
+
+```dosini
+MOLTIN_CLIENT_ID=
+MOLTIN_CLIENT_SECRET=
+MOLTIN_WEBHOOK_SECRET=
+ZENDESK_INTEGRATION_EMAIL=
+ZENDESK_INTEGRATION_SECRET=
+
+```
+
+Find your `MOLTIN_CLIENT_ID` and `MOLTIN_CLIENT_SECRET` inside of your [moltin Dashboard](https://dashboard.moltin.com)'s API keys.
+
+`MOLTIN_WEBHOOK_SECRET` can be anything you want.
+
+TODO: add instructions on ZENDESK keys above
+
+## 📦 Package
+
+Run the following command to build the app
+
+```bash
+yarn install
+```
+
+Start the development server
+
+```bash
+yarn dev
+```
+
+The server will typically start on PORT `3000`, if not, make a note for the next step.
+
+Start ngrok (change ngrok port below from 3000 if yarn dev deployed locally on different port above)
+
+```bash
+ngrok http 3000
+```
+
+Make a note of the https `ngrok URL` provided.
+
+## ⛽️ Usage
+
+Next head over to the [moltin Webhook Settings](https://dashboard.moltin.com/app/settings/integrations) area, add a new integration (`Settings > Integrations` and click `Create`).
+
+Enter any name and description for your Integration. Moltin recommends you prefix the name with `DEVELOPMENT:` for any testing.
+
+Next, enter the `ngrok URL` from above and `MOLTIN_WEBHOOK_SECRET` that you saved inside `.env`.
+
+![URL and Secret Key](https://user-images.githubusercontent.com/950181/52846929-ca957980-3102-11e9-9a20-23b8139767ee.png)
+
+Now finally you'll want to configure what Moltin Observables will cause this webhook to be invoked. In this example we want to monitor the `Order` observable and select the `Created`,`Fulfilled` and `Paid/Captured` box.
+
+![Observes selection](https://user-images.githubusercontent.com/950181/52851227-76dc5d80-310d-11e9-9dff-70b7daaf21e8.png)
+
+Click Save to register your new Webhook with Moltin.
+
+## 🚀 Deploy
+
+You can easily deploy this function to [now](https://now.sh).
+
+_Contact [Adam Grohs](https://www.linkedin.com/in/adamgrohs/) @ [Particular.](https://uniquelyparticular.com) for any questions._
---
+++ index.js
@@ -0,0 +1,156 @@
+const { json, send } = require('micro')
+const fetch = require('node-fetch')
+const { createClient } = require('@moltin/request')
+const moltin = new createClient({
+  client_id: process.env.MOLTIN_CLIENT_ID,
+  client_secret: process.env.MOLTIN_CLIENT_SECRET,
+  application: 'demo-sync-stripe-to-moltin'
+})
+const cors = require('micro-cors')({
+  allowMethods: ['POST'],
+  exposeHeaders: ['x-moltin-secret-key'],
+  allowHeaders: [
+    'x-moltin-secret-key',
+    'x-forwarded-proto',
+    'X-Requested-With',
+    'Access-Control-Allow-Origin',
+    'X-HTTP-Method-Override',
+    'Content-Type',
+    'Authorization',
+    'Accept'
+  ]
+})
+
+const _toJSON = error => {
+  return !error
+    ? ''
+    : Object.getOwnPropertyNames(error).reduce(
+        (jsonError, key) => {
+          return { ...jsonError, [key]: error[key] }
+        },
+        { type: 'error' }
+      )
+}
+
+const _toCamelcase = string => {
+  return !string
+    ? ''
+    : string.replace(
+        /\w\S*/g,
+        word => `${word.charAt(0).toUpperCase()}${word.substr(1).toLowerCase()}`
+      )
+}
+
+process.on('unhandledRejection', (reason, p) => {
+  console.error(
+    'Promise unhandledRejection: ',
+    p,
+    ', reason:',
+    JSON.stringify(reason)
+  )
+})
+
+module.exports = cors(async (req, res) => {
+  if (req.method === 'OPTIONS') {
+    return send(res, 200, 'ok!')
+  }
+  if (
+    (await req.headers['x-moltin-secret-key']) !=
+    process.env.MOLTIN_WEBHOOK_SECRET
+  )
+    return send(res, 401)
+
+  // try {
+  const { triggered_by, resources: body } = await json(req)
+
+  const {
+    data: { type: observable, id }
+  } = JSON.parse(body)
+
+  const event = triggered_by.split('.')[1] //event is 'order', trigger is `created`,`updated`,`fulfilled` or `paid`
+
+  if (observable === 'order') {
+    // just locking down to orders to protect code below
+    const {
+      data: {
+        status: order_status,
+        payment: payment_status,
+        shipping: shipping_status,
+        customer: { name: customer_name, email },
+        meta: {
+          display_price: {
+            with_tax: { formatted: total_paid }
+          }
+        },
+        relationships: {
+          items: { data: items },
+          customer: {
+            data: { id: customer_id }
+          }
+        }
+      }
+    } = await moltin.get(`${observable}s/${id}`)
+
+    const payload = {
+      profile: {
+        source: 'support',
+        identifiers: {
+          moltin_id: customer_id,
+          email: email
+        }
+      },
+      event: {
+        source: 'moltin',
+        type: `moltin-${observable}-${event}`,
+        description: _toCamelcase(`${observable} ${event}`),
+        properties: {
+          'Customer Name': customer_name,
+          'Order ID': id,
+          'Order Status': order_status,
+          'Order Total': total_paid,
+          'Payment Status': payment_status,
+          'Shipping Status': shipping_status
+        }
+      }
+    }
+    console.log('payload', payload)
+
+    fetch('https://d3v-uniquelyparticular.zendesk.com/api/cdp/v2/track', {
+      headers: {
+        Authorization: `Basic ${Buffer.from(
+          `${process.env.ZENDESK_INTEGRATION_EMAIL}/token:${
+            process.env.ZENDESK_INTEGRATION_SECRET
+          }`
+        ).toString('base64')}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify(payload)
+    })
+      .then(response => {
+        if (response.ok && response.status < 299) {
+          return send(res, 200, JSON.stringify({ received: true }))
+        } else {
+          return send(res, 500, 'Error')
+        }
+      })
+      .catch(error => {
+        console.log('error', error)
+        const jsonError = _toJSON(error)
+        return send(
+          res,
+          jsonError.type === 'StripeSignatureVerificationError' ? 401 : 500,
+          jsonError
+        )
+      })
+  }
+  // } catch (error) {
+  //   const jsonError = _toJSON(error)
+  //   return send(
+  //     res,
+  //     500,
+  //     jsonError
+  //   )
+  // }
+})
