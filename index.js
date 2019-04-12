f5f482fbba7fef4139d6d1e91db689815520877e
Adam Grohs <adamgrohs@Adams-MacBook-Air.local>
2019-04-12 13:19:05 -0500

    fix: changed OPTIONS response

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ process.on('unhandledRejection', (reason, p) => {
 
 module.exports = cors(async (req, res) => {
   if (req.method === 'OPTIONS') {
-    return send(res, 200, 'ok!')
+    return send(res, 204)
   }
   if (
     (await req.headers['x-moltin-secret-key']) !=
