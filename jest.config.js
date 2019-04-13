f7731873ff1ba894411f87d8ca12719ac1d5a853
Adam Grohs <adamgrohs@Adams-MacBook-Air.local>
2019-04-12 20:09:39 -0500

    added back testURL to jest config

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   roots: ['<rootDir>/src', '<rootDir>/tests'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.jsx?$',
   moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
-  verbose: true
+  verbose: true,
+  testURL: 'http://localhost/'
 }
