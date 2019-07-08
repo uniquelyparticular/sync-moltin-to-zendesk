71b5dc1e7fe0438394ae50fb7877511e89c727db
Adam Grohs <adamgrohs@Adams-MacBook-Air-2.local>
2019-07-08 01:10:36 -0400

    fix: set created_at to updated_at

--- src/index.js
+++ src/index.js
@@ -110,6 +110,7 @@ module.exports = cors(async (req, res) => {
             source: 'moltin',
             type: `${observable}-${trigger}`,
             description: _toCamelcase(`${observable} ${trigger}`),
+            created_at: updated_at,
             properties: {
               'Customer Name': customer_name,
               'Order ID': observable_id,
@@ -117,16 +118,16 @@ module.exports = cors(async (req, res) => {
               'Order Total': total_paid,
               'Order Created': created_at,
               'Order Updated': updated_at,
-              'Order Number': order_number,
-              'Order Items': order_items.filter(
-                item => item.type === 'cart_item'
-              ).length,
+              // 'Order Number': `${order_number}`,
+              // 'Order Items': `${order_items.filter(
+              //   item => item.type === 'cart_item'
+              // ).length}`,
               'Payment Status': payment_status,
               'Shipping Status': shipping_status
             }
           }
         }
-        console.log('payload', payload)
+        // console.log('payload', payload)
 
         fetch(
           `https://${process.env.ZENDESK_SUBDOMAIN}.zendesk.com/api/sunshine/track`,
